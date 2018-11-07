f97ba9548019abdb9a2c6719dee24b9cf9c1842c
Juan José Madrigal <juanjomadrigal326@gmail.com>
2018-11-07 21:08:18 +0100

    Update pascal_triangle_multiples.py

--- sage/pascal_triangle_multiples.py
+++ sage/pascal_triangle_multiples.py
@@ -5,6 +5,6 @@ def _(p=2, n=64):
     
     for i in range(n):
         for j in range(i+1):
-            g += circle((j-i/2,-sqrt(3)*i/2), 0.4, fill=True, rgbcolor='steelblue' if binomial(i,j)%p==0 else 'whitesmoke')
+            g += circle((j-i/2.0,-sqrt(3)*i/2), 0.4, fill=True, rgbcolor='steelblue' if binomial(i,j)%p==0 else 'whitesmoke')
             
     show(g, axes=False)
